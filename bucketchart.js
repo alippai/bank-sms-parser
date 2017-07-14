bcc3119bbb37790fe954024213cc5d3574841a4c
Adam Lippai <adam@rigo.sk>
2017-07-14 21:48:21 +0200

    Column chart options

--- bucketchart.js
+++ bucketchart.js
@@ -6,6 +6,9 @@ export default function (withCategory) {
     title: {
       text: 'All'
     },
+    xAxis: {
+      categories: ['< 1000', '< 5000', '< 10000', '< 50000', '< 100000', '> 100000']
+    },
     tooltip: {
       pointFormat: '{series.name}: <b>{point.y:.0f}</b> ({point.percentage:.1f}%)'
     },
@@ -19,26 +22,25 @@ export default function (withCategory) {
       }
     },
     series: [{
-      name: 'Expenses',
       colorByPoint: true,
       data: [{
         name: '< 1000',
-        y: -1 * withCategory.filter(x => x.amount >= -1000 && x.amount < 0).reduce((prev, curr) => prev + curr.amount, 0),
+        y: -1 * withCategory.filter(x => x.amount >= -1000 && x.amount < 0).reduce((prev, curr) => prev + Math.round(curr.amount), 0),
       }, {
         name: '< 5000',
-        y: -1 * withCategory.filter(x => x.amount >= -5000 && x.amount < -1000).reduce((prev, curr) => prev + curr.amount, 0),
+        y: -1 * withCategory.filter(x => x.amount >= -5000 && x.amount < -1000).reduce((prev, curr) => prev + Math.round(curr.amount), 0),
       }, {
         name: '< 10000',
-        y: -1 * withCategory.filter(x => x.amount >= -10000 && x.amount < -5000).reduce((prev, curr) => prev + curr.amount, 0),
+        y: -1 * withCategory.filter(x => x.amount >= -10000 && x.amount < -5000).reduce((prev, curr) => prev + Math.round(curr.amount), 0),
       }, {
         name: '< 50000',
-        y: -1 * withCategory.filter(x => x.amount >= -50000 && x.amount < -10000).reduce((prev, curr) => prev + curr.amount, 0),
+        y: -1 * withCategory.filter(x => x.amount >= -50000 && x.amount < -10000).reduce((prev, curr) => prev + Math.round(curr.amount), 0),
       }, {
         name: '< 100000',
-        y: -1 * withCategory.filter(x => x.amount >= -100000 && x.amount < -50000).reduce((prev, curr) => prev + curr.amount, 0),
+        y: -1 * withCategory.filter(x => x.amount >= -100000 && x.amount < -50000).reduce((prev, curr) => prev + Math.round(curr.amount), 0),
       }, {
         name: '> 100000',
-        y: -1 * withCategory.filter(x => x.amount < -100000).reduce((prev, curr) => prev + curr.amount, 0),
+        y: -1 * withCategory.filter(x => x.amount < -100000).reduce((prev, curr) => prev + Math.round(curr.amount), 0),
       }]
     }]
   });
--- build/fallback.js
+++ build/fallback.js
@@ -316,6 +316,9 @@ var drawBucketChart = function (withCategory) {
     title: {
       text: 'All'
     },
+    xAxis: {
+      categories: ['< 1000', '< 5000', '< 10000', '< 50000', '< 100000', '> 100000']
+    },
     tooltip: {
       pointFormat: '{series.name}: <b>{point.y:.0f}</b> ({point.percentage:.1f}%)'
     },
@@ -329,26 +332,25 @@ var drawBucketChart = function (withCategory) {
       }
     },
     series: [{
-      name: 'Expenses',
       colorByPoint: true,
       data: [{
         name: '< 1000',
-        y: -1 * withCategory.filter(x => x.amount >= -1000 && x.amount < 0).reduce((prev, curr) => prev + curr.amount, 0),
+        y: -1 * withCategory.filter(x => x.amount >= -1000 && x.amount < 0).reduce((prev, curr) => prev + Math.round(curr.amount), 0),
       }, {
         name: '< 5000',
-        y: -1 * withCategory.filter(x => x.amount >= -5000 && x.amount < -1000).reduce((prev, curr) => prev + curr.amount, 0),
+        y: -1 * withCategory.filter(x => x.amount >= -5000 && x.amount < -1000).reduce((prev, curr) => prev + Math.round(curr.amount), 0),
       }, {
         name: '< 10000',
-        y: -1 * withCategory.filter(x => x.amount >= -10000 && x.amount < -5000).reduce((prev, curr) => prev + curr.amount, 0),
+        y: -1 * withCategory.filter(x => x.amount >= -10000 && x.amount < -5000).reduce((prev, curr) => prev + Math.round(curr.amount), 0),
       }, {
         name: '< 50000',
-        y: -1 * withCategory.filter(x => x.amount >= -50000 && x.amount < -10000).reduce((prev, curr) => prev + curr.amount, 0),
+        y: -1 * withCategory.filter(x => x.amount >= -50000 && x.amount < -10000).reduce((prev, curr) => prev + Math.round(curr.amount), 0),
       }, {
         name: '< 100000',
-        y: -1 * withCategory.filter(x => x.amount >= -100000 && x.amount < -50000).reduce((prev, curr) => prev + curr.amount, 0),
+        y: -1 * withCategory.filter(x => x.amount >= -100000 && x.amount < -50000).reduce((prev, curr) => prev + Math.round(curr.amount), 0),
       }, {
         name: '> 100000',
-        y: -1 * withCategory.filter(x => x.amount < -100000).reduce((prev, curr) => prev + curr.amount, 0),
+        y: -1 * withCategory.filter(x => x.amount < -100000).reduce((prev, curr) => prev + Math.round(curr.amount), 0),
       }]
     }]
   });
