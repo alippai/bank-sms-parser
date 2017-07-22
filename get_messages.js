0fb2a9328176cb1f9f96e99f0cceb7cae6ce0e09
Adam Lippai <adam@rigo.sk>
2017-07-22 17:06:54 +0200

    Add cache

--- get_messages.js
+++ get_messages.js
@@ -1,8 +1,24 @@
-export default function (messageIds) {
-    return Promise.all(messageIds.map(message => {
+import cache from './idb.js';
+
+export default async function (messageIds) {
+    const result = [];
+    const messagesToGet = [];
+    for (const messageId of messageIds) {
+      const message = await cache.get(messageId.id);
+      if (message === undefined) {
+        messagesToGet.push(messageId);
+      } else {
+        result.push(message);
+      }
+    }
+
+    const getResult = Promise.all(messageIds.map(message => {
         return new Promise(resolve => gapi.client.gmail.users.messages.get({
             'userId': 'me',
             'id': message.id,
-        }).then(response => resolve(response.result.snippet)));
-    }))
+        }).then(response => response.result.snippet).then(snippet => {
+          cache.set(message.id, snippet).then(() => resolve(snippet));
+        }));
+    }));
+    return [...result, ...getResult];
 }
--- sw.js
+++
@@ -1,4 +0,0 @@
-self.addEventListener('fetch', event => {
-  console.log('Handling fetch event for', event.request.url);
-});
-
